49765812e8853270614a7d63985c1e1c217ba1fd
Faheem <pheeca@hotmail.com>
2019-07-28 16:29:56 +0500

    initial

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0f,0f,10f);
+    }
+}
